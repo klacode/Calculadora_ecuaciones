33f0cb4e396aa6bf85907762294b3c27f8dd50d7
klacode <50679814+klacode@users.noreply.github.com>
2019-05-15 22:35:03 +0200

    Add files via upload

---
+++ Calculadora_ecuaciones.py
@@ -0,0 +1,37 @@
+#cabecera
+
+print(""" 
+..######.....###....##........######..##.....##.##..........###....########...#######..########.....###......
+.##....##...##.##...##.......##....##.##.....##.##.........##.##...##.....##.##.....##.##.....##...##.##.....
+.##........##...##..##.......##.......##.....##.##........##...##..##.....##.##.....##.##.....##..##...##....
+.##.......##.....##.##.......##.......##.....##.##.......##.....##.##.....##.##.....##.########..##.....##...
+.##.......#########.##.......##.......##.....##.##.......#########.##.....##.##.....##.##...##...#########...
+.##....##.##.....##.##.......##....##.##.....##.##.......##.....##.##.....##.##.....##.##....##..##.....##...
+..######..##.....##.########..######...#######..########.##.....##.########...#######..##.....##.##.....##...
+ """)
+print("Author: @klacode")
+print("\nDe la forma Ax^2+Bx+C con números enteros")
+#Meter datos
+print("\nPon A:") 
+num_a=input()
+
+print("Pon B:")
+num_b=input()
+
+print("Pon C:")
+num_c=input()
+#convierte los numeros en entidades enteras
+numm_a=int(num_a)
+numm_b=int(num_b)
+numm_c=int(num_c)
+#Si la ecuacion no tiene solución 
+if numm_b**2-4*numm_a*numm_c<0 :
+	print("no tiene solución") 
+#Si la ecuacion tiene solucion hace las operaciones
+else:
+	print("\nSOLUCIONES")
+	import math
+	print((-numm_b+math.sqrt(numm_b**2-4*numm_a*numm_c))/(2*numm_a))
+	print((-numm_b-math.sqrt(numm_b**2-4*numm_a*numm_c))/(2*numm_a))
+
+
